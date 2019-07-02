e793abf344b23bff65ecf1979ac4c4f111db0359
duawoWkd <duawowkd@naver.com>
2019-07-02 16:52:19 +0900

    first

---
+++ src/com/biz/iolist/exec/IoEx_04.java
@@ -0,0 +1,21 @@
+package com.biz.iolist.exec;
+
+import com.biz.iolist.service.IolistService;
+
+public class IoEx_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IolistService ioService = new IolistService();
+		
+		while(true) {
+			
+			ioService.viewIolist();
+			
+			if(ioService.inserIO()) System.out.println("데이터 추가성공");
+			else System.out.println("데이터 추가 실패");
+		}
+
+	}
+
+}
